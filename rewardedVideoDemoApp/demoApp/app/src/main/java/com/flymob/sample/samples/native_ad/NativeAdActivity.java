7749a1b0125bf452b1dc8084477ef6715cf66399
Aleksey Baskakov <a.baskakov@macmini-baskakov.it.lan>
2016-12-28 17:08:36 +0300

    1.9.1

--- rewardedVideoDemoApp/demoApp/app/src/main/java/com/flymob/sample/samples/native_ad/NativeAdActivity.java
+++ rewardedVideoDemoApp/demoApp/app/src/main/java/com/flymob/sample/samples/native_ad/NativeAdActivity.java
@@ -23,7 +23,7 @@ import com.flymob.sdk.common.ads.native_ad.IFlyMobNativeAdListener;
 import java.util.Locale;
 
 public class NativeAdActivity extends AppCompatActivity {
-    private static final int ZONE_ID = 934105;
+    private static final int ZONE_ID = 841897;
     Toolbar mToolBar;
     View mButtonLoad;
     View mButtonShow;
